4554afa53ee393df1ebf26b9fa5ef796b2b5aaff
Andrew <32670729+mazalkov@users.noreply.github.com>
2023-03-17 10:02:39 +0000

    Create longestDigitsPrefix.py

---
+++ Code Arcade/Intro/Level 9/longestDigitsPrefix.py
@@ -0,0 +1,12 @@
+def solution(inputString):
+    
+    res = ""
+
+    for c in inputString:
+        if c.isdigit():
+            res += c
+            
+        else: break
+            
+            
+    return res
